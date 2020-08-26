f5753d01a2c27d20e78f2dc02cc06ea66763a106
Alan Temirov <alan.temirov@customertimes.com>
2020-08-26 10:44:13 +0300

    Initial commit

---
+++ Facts about cats/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  Facts about cats
+//
+//  Created by Alan on 26.08.2020.
+//  Copyright © 2020 Alan Temirov. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+}
+
---
+++ Facts about cats/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facts about cats
+//
+//  Created by Alan on 26.08.2020.
+//  Copyright © 2020 Alan Temirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
